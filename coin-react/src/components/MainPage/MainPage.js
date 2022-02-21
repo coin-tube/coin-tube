6e21afb74c1e3ad104e8e5fb9618209ac14ce9d3
gawon <rkdnddl23@gmail.com>
2022-02-21 19:07:26 +0900

    add MainPage component except backend process

--- coin-react/src/components/MainPage/MainPage.js
+++ coin-react/src/components/MainPage/MainPage.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../Header';
 import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
-
+import styled from 'styled-components';
 import YoutuberCard from './YoutuberCard';
 
 function MainPage() {
+  const [page, setPage] = useState(1);
+  const [Youtubers, setYoutubers] = useState([]);
+
+  const handleScroll = (e) => {
+    const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
+
+    console.log(scrollTop);
+    if (scrollHeight - scrollTop === clientHeight) {
+      setPage(prev => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    const loadYoutuber = async () => {
+      // const NewYoutubers = await ;
+      // setYoutubers((prev) => [...prev, ...NewYoutubers]);
+    };
+
+    loadYoutuber();
+  }, [page]);
+
   return (
     <div>
       <Header/>
-      Main
-      <YoutuberCard/>
+      <Container onScroll={handleScroll}>
+        {Youtubers && Youtubers.map((Youtuber) => <YoutuberCard/>)}
+      </Container>
     </div>
   );
 }
