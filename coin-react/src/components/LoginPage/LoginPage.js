abd69bcbfbf2a5b321e2665b16fea93630e7f2a8
yongsang <restart916@gmail.com>
2022-02-22 17:28:38 +0900

    로그인 안되는 문제 수정

--- coin-react/src/components/LoginPage/LoginPage.js
+++ coin-react/src/components/LoginPage/LoginPage.js
@@ -1,30 +1,46 @@
-import React, { useState, useEffect, useRef } from "react";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword  } from 'firebase/auth';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
-import {Link} from 'react-router-dom';
-import {useForm} from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 function LoginPage() {
-  const { register, watch, formState: { errors }, handleSubmit } = useForm({mode: "onChange"});
-  const [errorFromSubmit, setErrorFromSubmit] = useState("")
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ mode: 'onChange' });
+  const [errorFromSubmit, setErrorFromSubmit] = useState('');
   const [loading, setLoading] = useState(false);
   useEffect(() => {});
   const auth = getAuth();
 
   const onSubmit = async (data) => {
     try {
-        console.log("test")
-        setLoading(true)
-        await signInWithEmailAndPassword(auth, data.email, data.password);
-        setLoading(false)
+      // console.log('test', data);
+      setLoading(true);
+      const user = await signInWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      setLoading(false);
+      console.log('user', user);
     } catch (error) {
-        setErrorFromSubmit(error.message)
-        setLoading(false)
-        setTimeout(() => {
-            setErrorFromSubmit("")
-        }, 5000);
+      console.log(error);
+      setErrorFromSubmit(error.message);
+      setLoading(false);
+      setTimeout(() => {
+        setErrorFromSubmit('');
+      }, 5000);
     }
-  }
+  };
 
   const loginWithGoogle = async () => {
     try {
@@ -55,31 +71,47 @@ function LoginPage() {
 
   return (
     <div>
-      <div className="login-form" >
-        <div className= "login-head">Coin-Tube</div>
+      <div className="login-form">
+        <div className="login-head">Coin-Tube</div>
         <div className="baseline"></div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className= "acinput-container">
-            <input type="email" name="email" className="text-field" placeholder="email"
-            {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-            />{errors.email && errors.email.type === "required" && <p>This name field is required</p>}
-            {errors.email && errors.email.type === "maxLength" && <p>알맞는 이메일 형식이 아닙니다</p>}
-            <input type="password" name="pw" className="text-field" placeholder="password"/>
+          <div className="acinput-container">
+            <input
+              type="email"
+              name="email"
+              className="text-field"
+              placeholder="email"
+              {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+            />
+            {errors.email && errors.email.type === 'required' && (
+              <p>This name field is required</p>
+            )}
+            {errors.email && errors.email.type === 'maxLength' && (
+              <p>알맞는 이메일 형식이 아닙니다</p>
+            )}
+            <input
+              type="password"
+              name="pw"
+              className="text-field"
+              placeholder="password"
+              {...register('password', { required: true })}
+            />
           </div>
-          <input type="submit" value="Login" className="submit-btn"/>
-          <Link to="/register"><input type="submit" value="Signup" className="signup-btn"/></Link>
+          <input type="submit" value="Login" className="submit-btn" />
+          <Link to="/register">
+            <input type="submit" value="Signup" className="signup-btn" />
+          </Link>
         </form>
-        <div className="line">  info  </div>
+        <div className="line"> info </div>
         <div className="links">
-
-        {errorFromSubmit &&
-                    <p>{errorFromSubmit}</p>
-                }
-        <div>
-        <button onClick={loginWithGoogle} className="google-login-form">continue with google</button>
+          {errorFromSubmit && <p>{errorFromSubmit}</p>}
+          <div>
+            <button onClick={loginWithGoogle} className="google-login-form">
+              continue with google
+            </button>
+          </div>
+          <a href="#">비밀번호를 잊어버리셨나요?</a>
         </div>
-        <a href ="#">비밀번호를 잊어버리셨나요?</a>
-      </div>
       </div>
     </div>
   );
