36c18faa695d170082e485d821efcf69662b2548
chanhyeoKing <cksgurwkd12@naver.com>
2022-02-23 23:14:52 +0900

    update navigator ui, 2022-02-23

--- coin-react/src/components/Header.js
+++ coin-react/src/components/Header.js
@@ -10,27 +10,28 @@ function Header() {
     localStorage.removeItem('user');
     navigate('/');
   };
-
   return (
     <div>
-      Header
-      <div>
-        <a href="/">Main</a>
-        <br></br>
-
-        <br></br>
-        <a href="/register">register</a>
-        <br></br>
-        <a href="/main">main-real</a>
-        <br></br>
-
-        {user ? (
-          <a href="#" onClick={logout}>
-            logout
-          </a>
-        ) : (
-          <a href="/login">Login</a>
-        )}
+      <div class="inner">
+        <div class="nav_body">
+          <nav>
+            <ul class="nav-container">
+              <li class="nav-item"><a href="/">Main</a></li>
+              <li class="nav-item"><a href="/register">register</a></li>
+              <li class="nav-item"><a href="/admin">admin</a></li>
+              <li class="nav-item">            {user ? (
+              <a href="#" onClick={logout}>
+                logout
+              </a>
+            ) : (
+              <a href="/login">Login</a>
+            )}</li>
+            </ul>
+            <div>
+            <input class="search_input" type="text" placeholder="Search"></input>
+            </div>
+          </nav>
+        </div>
       </div>
     </div>
   );
